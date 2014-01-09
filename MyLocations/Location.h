b89098955193b6b42f1c33b5d24aaa338b9aadd0
John Hopkins <johnhopkins@gmx.co.uk>
2014-01-09 12:23:50 +0000

    Added the dat model with Core Data

---
+++ MyLocations/Location.h
@@ -0,0 +1,22 @@
+//
+//  Location.h
+//  MyLocations
+//
+//  Created by John Hopkins on 09/01/2014.
+//  Copyright (c) 2014 cocoacoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Location : NSManagedObject
+
+@property (nonatomic, retain) NSNumber *latitude;
+@property (nonatomic, retain) NSNumber *longitude;
+@property (nonatomic, retain) NSDate *date;
+@property (nonatomic, retain) NSString *locationDescription;
+@property (nonatomic, retain) NSString *category;
+@property (nonatomic, retain) CLPlacemark *placemark;
+
+@end
---
+++ MyLocations/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  MyLocations
+//
+//  Created by John Hopkins on 09/01/2014.
+//  Copyright (c) 2014 cocoacoda. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+@dynamic latitude;
+@dynamic longitude;
+@dynamic date;
+@dynamic locationDescription;
+@dynamic category;
+@dynamic placemark;
+
+@end
