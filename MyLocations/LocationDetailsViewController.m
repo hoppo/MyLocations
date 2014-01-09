3299e2339cdb2cc413903824c8b64c3d341390a9
John Hopkins <johnhopkins@gmx.co.uk>
2014-01-09 14:40:11 +0000

    Saves locations to CoreData

--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -9,6 +9,7 @@
 #import "LocationDetailsViewController.h"
 #import "CategoryPickerViewController.h"
 #import "HudView.h"
+#import "Location.h"
 
 @interface LocationDetailsViewController () <UITextViewDelegate>
 
@@ -25,6 +26,7 @@
 {
     NSString *_descriptionText;
     NSString *_categoryName;
+    NSDate *_date;
 }
 
 -(id)initWithCoder:(NSCoder *)aDecoder
@@ -32,6 +34,8 @@
     if ((self = [super initWithCoder:aDecoder])) {
         _descriptionText = @"";
         _categoryName = @"No Category";
+        _date = [NSDate date];
+        NSLog(@"The Date %@", _date);
     }
     return self;
 }
@@ -49,7 +53,7 @@
         self.addressLabel.text = @"No Address Found";
         }
         
-        self.dateLabel.text = [self formatDate:[NSDate date]];
+    self.dateLabel.text = [self formatDate:_date];
     
     UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard:)];
     gestureRecognizer.cancelsTouchesInView = NO; [self.tableView addGestureRecognizer:gestureRecognizer];
@@ -70,6 +74,21 @@
 {
     HudView *hudView = [HudView hudInView:self.navigationController.view animated:YES];
     hudView.text = @"Tagged";
+    
+    Location *location = [NSEntityDescription insertNewObjectForEntityForName:@"Location" inManagedObjectContext:self.managedObjectContext];
+    
+    location.locationDescription = _descriptionText;
+    location.category = _categoryName;
+    location.latitude = @(self.coordinate.latitude);
+    location.longitude = @(self.coordinate.longitude);
+    location.date = _date;
+    location.placemark = self.placemark;
+    
+    NSError *error;
+    if (![self.managedObjectContext save:&error]) {
+        NSLog(@"Error: %@", error);
+        abort();
+    }
     
     [self performSelector:@selector(closeScreen) withObject:nil afterDelay:0.6];
 }
