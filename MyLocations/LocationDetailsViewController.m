57b48bc8759f0ba34b151b2b1fb0b88ca105aae3
John Hopkins <johnhopkins@gmx.co.uk>
2014-01-14 16:26:20 +0000

    Save the photo’s, present them in the locations view and allow them to be changed.

--- MyLocations/LocationDetailsViewController.m
+++ MyLocations/LocationDetailsViewController.m
@@ -72,6 +72,16 @@
     
     if (self.locationToEdit != nil) {
         self.title = @"Edit Location";
+        
+        if ([self.locationToEdit hasPhoto])
+        {
+            UIImage *existingImage = [self.locationToEdit photoImage];
+            if (existingImage != nil)
+            
+            {
+                [self showImage:existingImage];
+            }
+        }
     }
     
     self.descriptionTextView.text = _descriptionText;
@@ -133,6 +143,7 @@
     } else {
         hudView.text = @"Tagged";
         location = [NSEntityDescription insertNewObjectForEntityForName:@"Location" inManagedObjectContext:self.managedObjectContext];
+        location.photoId = @-1;
     }
     
     location.locationDescription = _descriptionText;
@@ -141,6 +152,18 @@
     location.longitude = @(self.coordinate.longitude);
     location.date = _date;
     location.placemark = self.placemark;
+    
+    if (_image != nil) {
+        if (![location hasPhoto]) {
+            location.photoId = @([Location nextPhotoId]);
+        }
+        
+        NSData *data = UIImageJPEGRepresentation(_image, 0.5); NSError *error;
+        if (![data writeToFile:[location photoPath] options:NSDataWritingAtomic error:&error])
+        {
+            NSLog(@"Error writing file: %@", error);
+        }
+    }
     
     NSError *error;
     if (![self.managedObjectContext save:&error]) {
