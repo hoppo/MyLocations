d9dbdb2d33e993c4c8a5bc9a2c2b60b39f9aef7b
John Hopkins <johnhopkins@gmx.co.uk>
2014-01-24 10:37:16 +0000

    Photos in table view now displayed in circular form

--- MyLocations/LocationsViewController.m
+++ MyLocations/LocationsViewController.m
@@ -156,6 +156,10 @@
     UIView *selectionView = [[UIView alloc] initWithFrame:CGRectZero];
     selectionView.backgroundColor = [UIColor colorWithWhite:1.0f alpha:0.2f];
     locationCell.selectedBackgroundView = selectionView;
+    
+    locationCell.photoImageView.layer.cornerRadius = locationCell.photoImageView.bounds.size.width / 2.0f;
+    locationCell.photoImageView.clipsToBounds = YES;
+    locationCell.separatorInset = UIEdgeInsetsMake(0, 82, 0, 0);
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
